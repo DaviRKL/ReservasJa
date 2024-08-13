cb6b2b5f1fc933411c5e2a78ba51ee72e2c91101
DaviRKL <davirkl07@gmail.com>
2024-08-13 17:51:30 -0300

    Criacao do swagger

--- backend-reservas-ja/src/main.ts
+++ backend-reservas-ja/src/main.ts
@@ -1,9 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Reservas Já')
+    .setDescription('The Reservas Já API description')
+    .setVersion('1.0')
+    .build();
+  
+  const options: SwaggerDocumentOptions =  {
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string
+    ) => methodKey
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
